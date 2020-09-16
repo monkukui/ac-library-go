969012fbfc056eac0c918429a1946cb7c703cf93
nu50218 <nu_ll@icloud.com>
2020-09-16 12:18:07 +0900

    fenwicktree: add tests

--- fenwicktree/int_test.go
+++ fenwicktree/int_test.go
@@ -1,12 +1,21 @@
 package fenwicktree
 
 import (
+	"math/big"
 	"math/rand"
 	"testing"
 )
 
 func TestInt(t *testing.T) {
 	bit := NewInt(5)
+
+	// check if all values are zero
+	for i := 0; i < 5; i++ {
+		if bit.Sum(i, i+1) != 0 {
+			t.Fatal(i, bit.Sum(i, i+1))
+		}
+	}
+
 	// [1, 2, 3, 4, 5]
 	for i := 0; i < 5; i++ {
 		bit.Add(i, i+1)
@@ -19,6 +28,36 @@ func TestInt(t *testing.T) {
 	}
 	if bit.Sum(1, 3) != 5 {
 		t.FailNow()
+	}
+}
+
+// TestIntSumOverflow
+// (*Int).Sum returns the answer in $\bmod 2^{\mathrm{bit}}$, if overflowed.
+func TestIntSumOverflow(t *testing.T) {
+	const N = 5000
+	const intSize = 32 << (^uint(0) >> 63)
+
+	// mod := 1 << intSize
+	mod := big.NewInt(0).SetUint64(uint64((1 << intSize) - 1))
+	mod.Add(mod, big.NewInt(1))
+
+	// returns if x ≡ y (mod 2^intSize)
+	congruence := func(x, y *big.Int) bool {
+		xmod := big.NewInt(0).Mod(x, mod)
+		ymod := big.NewInt(0).Mod(y, mod)
+		return xmod.Cmp(ymod) == 0
+	}
+
+	bit := NewInt(N)
+	sum := big.NewInt(0)
+
+	for i := 0; i < N; i++ {
+		r := rand.Int()
+		bit.Add(i, r)
+		sum.Add(sum, big.NewInt(int64(r)))
+		if !congruence(sum, big.NewInt(int64(bit.Sum(0, i+1)))) {
+			t.FailNow()
+		}
 	}
 }
 
