e20ec0b16ac460da7051c8a95973cef8a1b0e03b
Aruaru0 <aruaru0@gmail.com>
2020-09-16 09:08:13 +0900

    add scc

---
+++ scc/sample/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/monkukui/ac-library-go/scc"
+)
+
+func out(x ...interface{}) {
+	fmt.Println(x...)
+}
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func getInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, 1000000)
+
+	n, m := getInt(), getInt()
+
+	g := scc.SccGraph(n)
+	for i := 0; i < m; i++ {
+		u, v := getInt(), getInt()
+		g.AddEdge(u, v)
+	}
+
+	scc := g.Scc()
+
+	out(len(scc))
+	for _, e := range scc {
+		fmt.Print(len(e))
+		for _, x := range e {
+			fmt.Print(" ", x)
+		}
+		out()
+	}
+}
