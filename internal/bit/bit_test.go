cc8e76e274aa32142da7dd77fab49ad5b938a486
nu50218 <nu_ll@icloud.com>
2020-10-07 11:12:28 +0900

    add TestBsf

--- internal/bit/bit_test.go
+++ internal/bit/bit_test.go
@@ -32,3 +32,30 @@ func TestCeilPow2(t *testing.T) {
 		}
 	}
 }
+
+func TestBsf(t *testing.T) {
+	tests := []struct {
+		n    uint
+		want int
+	}{
+		// https://github.com/atcoder/ac-library/blob/2088c8e2431c3f4d29a2cfabc6529fe0a0586c48/test/unittest/bit_test.cpp
+		{1, 0},
+		{2, 1},
+		{3, 0},
+		{4, 2},
+		{5, 0},
+		{6, 1},
+		{7, 0},
+		{8, 3},
+		{9, 0},
+		{1 << 30, 30},
+		{(1 << 31) - 1, 0},
+		{(1 << 31), 31},
+		{math.MaxUint32, 0},
+	}
+	for _, tt := range tests {
+		if got := Bsf(tt.n); got != tt.want {
+			t.Errorf("bsf(%v) = %d, want %v", tt.n, got, tt.want)
+		}
+	}
+}
