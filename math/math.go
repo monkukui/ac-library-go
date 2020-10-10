73b0ff87d0e8142f1ba90cad38967a0ee3eaa7a2
monkukui <monkukui1953@gmail.com>
2020-10-11 03:27:16 +0900

    Add mod pow

---
+++ math/math.go
@@ -0,0 +1,23 @@
+package math
+
+import internal "github.com/monkukui/ac-library-go/internal/math"
+
+func PowMod(x, n int64, m int) int64 {
+	if !(0 <= n && 1 <= m) {
+		panic("")
+	}
+	if m == 1 {
+		return 0
+	}
+	bt := internal.New(uint(m))
+	r := uint(1)
+	y := uint(internal.SafeMod(x, int64(m)))
+	for n > 0 {
+		if n&1 > 0 {
+			r = bt.Mul(r, y)
+		}
+		y = bt.Mul(y, y)
+		n >>= 1
+	}
+	return r
+}
