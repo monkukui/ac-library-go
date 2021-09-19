449f105b3a748265b71851730d2a03fb4fcd1b6f
monkukui <monkukui1953@gmail.com>
2021-09-19 17:44:47 +0900

    Add sample output program

--- main.go
+++ main.go
@@ -19,10 +19,19 @@ func main() {
 		return math.MaxInt64
 	}
 	rmq := segtree.New(op, e, 10)
+	fmt.Println("set d[", 1, "] = ", 20)
+	rmq.Set(1, 20)
 
-	fmt.Println(rmq.Get(3))
-	fmt.Println(rmq.Prod(2, 4))
+	fmt.Println("d[", 1, "] = ", rmq.Get(1))
+	fmt.Println("d[", 3, "] = ", rmq.Get(3))
+	fmt.Println("Prod(", 0, ",", 4, ") = ", rmq.Prod(0, 4))
+	fmt.Println(rmq.Prod(0, 4))
+
+	fmt.Println("set d[", 3, "] = ", 10)
 	rmq.Set(3, 10)
-	fmt.Println(rmq.Get(3))
-	fmt.Println(rmq.Prod(2, 4))
+
+	fmt.Println("d[", 1, "] = ", rmq.Get(1))
+	fmt.Println("d[", 3, "] = ", rmq.Get(3))
+	fmt.Println("Prod(", 0, ",", 2, ") = ", rmq.Prod(0, 2))
+	fmt.Println("Prod(", 0, ",", 4, ") = ", rmq.Prod(0, 4))
 }
