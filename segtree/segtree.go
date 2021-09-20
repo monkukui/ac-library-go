13c375f8739754386ee591a1fb791a834093f44a
monkukui <monkukui1953@gmail.com>
2021-09-20 16:32:07 +0900

    Impl segtree method without max_right and min_left

--- segtree/segtree.go
+++ segtree/segtree.go
@@ -3,6 +3,7 @@ package segtree
 import (
 	internal "github.com/monkukui/ac-library-go/internal/bit"
 )
+
 type SegTree struct {
 	n, size, log int
 	d            []interface{}
@@ -25,15 +26,15 @@ func NewBySlice(op func(a, b interface{}) interface{}, e func() interface{}, v [
 	n := len(v)
 	log := internal.CeilPow2(n)
 	size := 1 << log
-	d := make([]interface{}, 2 * size)
-	for i := 0; i < 2 * size; i++ {
+	d := make([]interface{}, 2*size)
+	for i := 0; i < 2*size; i++ {
 		d[i] = e()
 	}
 	for i := 0; i < n; i++ {
-		d[size + i] = v[i]
+		d[size+i] = v[i]
 	}
 	for i := size - 1; i >= 1; i-- {
-		d[i] = op(d[2 * i], d[2 * i + 1])
+		d[i] = op(d[2*i], d[2*i+1])
 	}
 
 	return &SegTree{
@@ -47,23 +48,49 @@ func NewBySlice(op func(a, b interface{}) interface{}, e func() interface{}, v [
 }
 
 func (s *SegTree) Set(p int, x interface{}) {
+	if p < 0 || s.n <= p {
+		panic("")
+	}
 	s.d[p] = x
+	p += s.size
+	s.d[p] = x
+	for i := 1; i <= s.log; i++ {
+		s.update(p >> i)
+	}
 }
 
 func (s *SegTree) Get(p int) interface{} {
-	return s.d[p]
+	if p < 0 || s.n <= p {
+		panic("")
+	}
+	return s.d[p + s.size]
 }
 
 func (s *SegTree) Prod(l, r int) interface{} {
-	v := s.e()
-	for i := l; i < r; i++ {
-		v = s.op(v, s.d[i])
+	if l < 0 || r < l || s.n < r {
+		panic("")
 	}
-	return v
+	sml, smr := s.e(), s.e()
+	l += s.size
+	r += s.size
+
+	for l < r {
+		if l & 1 > 0 {
+			sml = s.op(sml, s.d[l])
+			l++
+		}
+		if r & 1 > 0 {
+			r--
+			smr = s.op(s.d[r], smr)
+		}
+		l >>= 1
+		r >>= 1
+	}
+	return s.op(sml, smr)
 }
 
 func (s *SegTree) AllProd() interface{} {
-	return s.Prod(0, len(s.d))
+	return s.d[1]
 }
 
 func (s *SegTree) MaxRight(l int, f func(x interface{}) bool) int {
